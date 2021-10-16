07b4402619df7eb7dee62fbb70d4b1d5a88268f5
Arash Massoudieh <arashmassoudieh@gmail.com>
2021-10-16 09:31:46 -0400

    Populate Links list view done!

--- gridgenerator.cpp
+++ gridgenerator.cpp
@@ -9,6 +9,7 @@ GridGenerator::GridGenerator(MainWindow *parent) :
     ui->setupUi(this);
     mainwindow = parent;
     PopulateBlocks();
+    PopulateLinks(); 
 }
 
 GridGenerator::~GridGenerator()
@@ -50,7 +51,31 @@ void GridGenerator::PopulateBlocks()
 }
 void GridGenerator::PopulateLinks()
 {
+    ui->listWidgetBlocks->setIconSize(QSize(50, 50));
+    for (unsigned int i = 0; i < system()->GetAllLinkTypes().size(); i++)
+    {
 
+        QIcon icon;
+        if (QString::fromStdString(system()->GetModel(system()->GetAllLinkTypes()[i])->IconFileName()).contains("/"))
+        {
+            if (QFile::exists(QString::fromStdString(system()->GetModel(system()->GetAllLinkTypes()[i])->IconFileName())))
+                icon.addFile(QString::fromStdString(system()->GetModel(system()->GetAllLinkTypes()[i])->IconFileName()), QSize(), QIcon::Normal, QIcon::Off);
+        }
+        else
+        {
+            if (QFile::exists(QString::fromStdString(qApp->applicationDirPath().toStdString() + "/../../resources/Icons/" + system()->GetModel(system()->GetAllLinkTypes()[i])->IconFileName())))
+                icon.addFile(QString::fromStdString(qApp->applicationDirPath().toStdString() + "/../../resources/Icons/" + system()->GetModel(system()->GetAllLinkTypes()[i])->IconFileName()), QSize(), QIcon::Normal, QIcon::Off);
+
+        }
+
+        QListWidgetItem* list_item = new QListWidgetItem(icon, QString::fromStdString(system()->GetModel(system()->GetAllLinkTypes()[i])->Description()));
+        //qDebug()<<QString::fromStdString(system()->GetModel(system()->GetAllBlockTypes()[i])->Description());
+        //string _text = system()->GetModel(system()->GetAllBlockTypes()[i])->Description();
+        //QString text = QString::fromStdString(system()->GetModel(system()->GetAllBlockTypes()[i])->Description());
+        list_item->setToolTip(QString::fromStdString(system()->GetModel(system()->GetAllLinkTypes()[i])->Description()));
+        list_item->setText(QString::fromStdString(system()->GetModel(system()->GetAllLinkTypes()[i])->Description()));
+        ui->listWidgetLinks->addItem(list_item);
+    }
 }
 
 System * GridGenerator::system()
