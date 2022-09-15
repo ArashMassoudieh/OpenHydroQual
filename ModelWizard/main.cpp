3b58d0079732ddd5702281caf6fcca43ff8b011e
Arash Massoudieh <arashmassoudieh@gmail.com>
2022-09-15 12:52:01 -0400

    Mac

--- ModelWizard/main.cpp
+++ ModelWizard/main.cpp
@@ -1,10 +1,23 @@
-#include "ModelWizard.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ModelWizard w;
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "ModelWizard_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
     w.show();
     return a.exec();
 }
